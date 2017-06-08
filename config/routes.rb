5c0412c08ddedc1546fda8d293fe3ce068619ed7
Sergio Velásquez <sy.velasquez10@uniandes.edu.co>
2017-06-08 01:44:25 -0500

    cambios iniciales front - vista de estudiantes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :jobs
   resources :roles
   resources :volunteers
+  root :to => "volunteers#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
